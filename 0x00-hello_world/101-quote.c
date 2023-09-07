e739b154792de9a057f4c48be1640ecd4948331a
Omaraiman <omaraiman4444@gmail.com>
2023-09-08 02:10:21 +0300

    cfa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -S $CFILE -o `echo $CFILE | cut -d. -f1`.s
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ * DESCRIPTION : print without use printf or puts
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char que[] = "and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n";
+
+	write(1, que, 59);
+	return (1);
+}
