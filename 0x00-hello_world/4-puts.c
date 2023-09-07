89af346539c7f341076d309673385581ff282de2
Omaraiman <omaraiman4444@gmail.com>
2023-09-08 01:29:46 +0300

    axc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
- * main -entry point
- * 
- * Description: A C program that prints with put funcition
- * 
- * Return: Alawys 0 (Success)
+* main -entry point
+* 
+* Description: A C program that prints with put funcition
+* 
+* Return: Alawys 0 (Success)
 */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
