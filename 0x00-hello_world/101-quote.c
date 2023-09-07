4f0a61a7b0a173ab59d26d9ab6ebffa2ea085de4
Omaraiman <omaraiman4444@gmail.com>
2023-09-08 02:14:25 +0300

    csda

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
+#include<stdio.h>
 
 /**
- * main -Entry point
- * DESCRIPTION : print without use printf or puts
- * Return: 1 (not success)
+ * main - Entry point
+ * DESCRIPTION: print without use printf or puts
+ * Return: Always 1 (error)
 */
 
 int main(void)
 {
-	char que[] = "and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n";
+	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, que, 59);
 	return (1);
