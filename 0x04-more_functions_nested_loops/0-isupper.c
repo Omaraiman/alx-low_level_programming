d1c57d34bcdc65a4c30608b97d3123af85b3f0e3
Omaraiman <omaraiman4444@gmail.com>
2023-09-14 21:42:14 +0300

    bv

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putcahr.c"
+
+/**
+*main - return valu
+*
+*Desctiption: A C program that return 1 or 0 in fuction called isupper
+*
+*Return: 0 , 1
+*/
+
+int isupper(void)
+{
+int count;
+int d;
+for (count = 'A'; count <= 'Z'; count++)
+{
+if (d != count)
+{
+return (1);
+}
+return (0);
+}
+}
