a444389cd6fcbc36afaef9540ed973b621c4a821
Omaraiman <omaraiman4444@gmail.com>
2023-09-14 21:57:46 +0300

    vb

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,16 @@
 #include "main.h"
-#include "_putcahr.c"
 
 /**
-*main - return valu
-*@c: char to check
-*
-*Desctiption: A C program that return 1 or 0 in fuction called isupper
-*
-*Return: 0 , 1
-*/
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
-_putchar("%d\n", c)
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
