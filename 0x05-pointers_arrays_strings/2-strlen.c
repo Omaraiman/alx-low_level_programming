72e867abf6d1654cd9db5e6f7d435894d21c4480
Omaraiman <omaraiman4444@gmail.com>
2023-09-19 21:08:08 +0300

    task3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length of array = i
+ */
+
+int _strlen(char *s)
+{
+	int s[] = "My first strlen!";
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
+}
