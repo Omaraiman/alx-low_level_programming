e8a8cfe2611b7bf56925e33b65adf356821936fb
Omaraiman <omaraiman4444@gmail.com>
2023-09-19 21:40:47 +0300

    task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar("\n");
+}
