81bfd908e046f561821a6aa2c84332ba5a5d6e44
Omaraiman <omaraiman4444@gmail.com>
2023-10-03 02:02:35 +0300

    tsa

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,32 +10,35 @@
  *
  * Return: 0 in success, 1 in Error.
  **/
+
 int main(int argc, char *argv[])
 {
-	int i, result, a;
-
-	result = 0;
-
-	if (argc > 0)
-	{
-		for (i = 0; argc > i; i++)
-		{
-			a = atoi(argv[i]);
-			if (!isdigit(a))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				result = result + a;
-			}
-		}
-	}
-	else
+	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", result);
+
+	int sum = 0;
+	int i = 1
+
+	for (; i < argc; i++)
+	{
+		char *endptr;
+		long num;
+
+		num = strtol(argv[i], &endptr, 10);
+
+		if (*endptr != '\0' || num < 0 || num > INT_MAX)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += (int)num;
+	}
+
+	printf("%d\n", sum);
+
 	return (0);
 }
