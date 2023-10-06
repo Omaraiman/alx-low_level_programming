4ce10820356bdfaae92a6db7152c78e9189d874d
Omaraiman <omaraiman4444@gmail.com>
2023-10-07 01:46:27 +0300

    fd

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = "\0";
 
 	size1 = strlen(s1);
 	size2  = strlen(s2);
