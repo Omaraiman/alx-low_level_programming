7b40a692b01d04f12647f0d2cd19550cdd716f3b
Omaraiman <omaraiman4444@gmail.com>
2023-10-10 18:16:35 +0300

    t

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define NAME printf("print file name: %s\n", NAME)
+
+/**
+ * main -  program that prints the name of the file it was compiled from
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	PRINT_FILE_NAME;
+	return (0);
+}
