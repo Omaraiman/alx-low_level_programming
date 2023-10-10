f41aec1ff36eaff45ccd8d99875ae0db9d1af152
Omaraiman <omaraiman4444@gmail.com>
2023-10-10 18:50:13 +0300

    t

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-
-#define FILE_NAME printf("%s\n", __FILE__)
+#define FILE_NAME printf("print file name: %s\n", __FILE__)
 
 /**
  * main -  program that prints the name of the file it was compiled from
