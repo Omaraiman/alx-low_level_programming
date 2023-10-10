342c8c95d4fa5132a8239dfb732c94f9dfd9894b
Omaraiman <omaraiman4444@gmail.com>
2023-10-10 20:55:57 +0300

    t

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
