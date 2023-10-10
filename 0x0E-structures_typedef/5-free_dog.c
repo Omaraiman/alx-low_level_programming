f4a87a95e47dc3c0cfc90af46e4466917999a70e
Omaraiman <omaraiman4444@gmail.com>
2023-10-11 00:21:14 +0300

    teaa

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ * free_dog - a short description of what the function does
+ * @d: a short description of the parameter
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+			if (d->age != NULL)
+		{
+			free(d->age);
+		}
+			if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
