ece869d6e5f0ee4617dd880101bf4c602cdaf53b
Omaraiman <omaraiman4444@gmail.com>
2023-10-12 01:01:59 +0300

    trs

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include "_putchar.c"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to the function to be used to print the name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		while (*name != '\0')
+		{
+			f(*name);
+			name++;
+		}
+	}
+}
