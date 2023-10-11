fa147c61f99e5611e7210274e0f2f6520317ee2f
Omaraiman <omaraiman4444@gmail.com>
2023-10-12 01:35:12 +0300

    test

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,36 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
- * print_error_and_exit - Prints an error message and exits the program.
- * @status: The exit status.
+ * print_error_and_exit - Prints an error message and exits the program
+ * @status: The exit status
  */
 void print_error_and_exit(int status)
 {
-	char *error = "Error\n";
-
-	while (*error)
-		_putchar(*error++);
-	exit(status);
+    printf("Error\n");
+    exit(status);
 }
+
 /**
- * print_opcodes - Prints the opcodes of this function.
- * @start: The start of the memory area to print.
- * @size: The number of bytes to print.
+ * print_opcodes - Prints the opcodes of a function
+ * @start: A pointer to the start of the function
+ * @size: The number of bytes to print
  */
 void print_opcodes(char *start, int size)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < size; i++)
-	{
-		_putchar("0123456789abcdef"[(start[i] >> 4) & 0xF]);
-		_putchar("0123456789abcdef"[start[i] & 0xF]);
-		if (i < size - 1)
-			_putchar(' ');
-	}
-	_putchar('\n');
+    for (i = 0; i < size; i++)
+    {
+        printf("%02hhx", start[i]);
+        if (i < size - 1)
+            printf(" ");
+    }
+    printf("\n");
 }
+
 /**
- * main - Entry point
+ * main - Prints the opcodes of its own main function
  * @argc: The number of command line arguments
  * @argv: The command line arguments
  *
@@ -38,15 +39,17 @@ void print_opcodes(char *start, int size)
  */
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-		print_error_and_exit(1);
+    int size;
 
-	int size = atoi(argv[1]);
+    if (argc != 2)
+        print_error_and_exit(1);
 
-	if (size < 0)
-		print_error_and_exit(2);
+    size = atoi(argv[1]);
 
-	print_opcodes((char *)main, size);
+    if (size < 0)
+        print_error_and_exit(2);
 
-	return (0);
+    print_opcodes((char *)main, size);
+
+    return (0);
 }
