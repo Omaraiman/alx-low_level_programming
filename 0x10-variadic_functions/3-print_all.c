760f5edaf710c0dd706280712a2ad32be9945124
Omaraiman <omaraiman4444@gmail.com>
2023-10-13 04:29:46 +0300

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+
 /**
  * print_all - Prints all arguments passed to it
  * @format: The format to print the arguments
  * Return: Nothing
  */
+
 void print_all(const char * const format, ...)
 {
 	va_list count;
