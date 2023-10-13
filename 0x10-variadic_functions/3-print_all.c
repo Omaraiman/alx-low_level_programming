15cfc570e84352543097ece067aff965f7cf5ced
Omaraiman <omaraiman4444@gmail.com>
2023-10-13 05:07:42 +0300

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,8 +16,6 @@ void print_all(const char * const format, ...)
 
 	va_start(count, format);
 
-	if (format)
-	{
 		while (format[i])
 		{
 			switch (format[i])
@@ -44,7 +42,6 @@ void print_all(const char * const format, ...)
 			sep = ", ";
 			i++;
 		}
-	}
 
 	printf("\n");
 	va_end(count);
